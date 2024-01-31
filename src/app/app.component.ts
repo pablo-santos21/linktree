fb3f8d3bf397fc285a7e76cf5256b6eda35e422a
DESKTOP-FUQ1C3A\HOME <binho.bgu@gmail.com>
2024-01-30 22:43:43 -0300

    adicionando telefone e pix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { ClipboardService } from 'ngx-clipboard';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'lintree-clone';
+  constructor(private _clipboardService: ClipboardService) {}
+
+  callServiceToCopy() {
+    this._clipboardService.copy('14856583735');
+    window.alert('Chave copiada!');
+  }
 }
--- src/app/components/button-redes/button-redes.component.ts
+++ src/app/components/button-redes/button-redes.component.ts
@@ -1,13 +1,14 @@
 import { Component, Input } from '@angular/core';
+import { NgModel } from '@angular/forms';
 
 @Component({
   selector: 'app-button-redes',
   templateUrl: './button-redes.component.html',
-  styleUrls: ['./button-redes.component.css']
+  styleUrls: ['./button-redes.component.css'],
 })
 export class ButtonRedesComponent {
-  @Input() linkUrl : string = "";
-  @Input() title : string = "";
-  @Input() image : string = "";
-
+  @Input() linkUrl?: string = '';
+  @Input() title: string = '';
+  @Input() image: string = '';
+  @Input() copy: string = 'callServiceToCopy()';
 }
